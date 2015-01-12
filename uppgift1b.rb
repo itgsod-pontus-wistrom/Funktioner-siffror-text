cd56b6ae70fe634d5fb3622965251d6032f058f3
itgsod-pontus-wistrom <pontus.wistrom@elev.it-gymnasiet.se>
2015-01-12 13:38:45 +0100

    uppgift1b

---
+++ uppgift1b.rb
@@ -0,0 +1,10 @@
+puts "Jag har 8st äpplen, 12st bananer, 7st paprikor och 13 styckna gurkor"
+
+äpplen=8
+bananer=12
+paprikor=7
+gurkor=13
+
+puts "Jag har totalt #{äpplen+bananer+paprikor+gurkor}st frukter"
+puts "Varje person får #{(äpplen+bananer+paprikor+gurkor)/4}st nyttigheter var"
+
